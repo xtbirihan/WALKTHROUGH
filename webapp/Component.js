d0817a6687cf7623db40b5fb34dab8b0ecd7272f
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-20 13:32:36 +0000

    Sample: Walkthrough - Step 30 - Routing and Navigation

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,31 +2,33 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel"
- ], function (UIComponent, JSONModel, ResourceModel) {
+], function (UIComponent, JSONModel, ResourceModel) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata : {
+        metadata: {
             interfaces: ["sap.ui.core.IAsyncContentCreation"],
             manifest: "json"
-       },
-       init : function () {
-          // call the init function of the parent
-          UIComponent.prototype.init.apply(this, arguments);
-          // set data model
-          var oData = {
-             recipient : {
-                name : "World"
-             }
-          };
-          var oModel = new JSONModel(oData);
-          this.setModel(oModel);
- 
-          // set i18n model
-          var i18nModel = new ResourceModel({
-             bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-          });
-          this.setModel(i18nModel, "i18n");
-       }
+        },
+        init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // set data model
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+
+            // set i18n model
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+            });
+            this.setModel(i18nModel, "i18n");
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
+        }
     });
- });
- 
+});
--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -25,6 +25,10 @@ sap.ui.define([
 			var oBinding = oList.getBinding("items");
 			oBinding.filter(aFilter);
 
-        }
+        },
+        onPress: function (oEvent) {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail");
+		}
     });
 });
