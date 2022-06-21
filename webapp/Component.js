8832a3b2e7db69d9a61fbdb115af806c9d19fc3c
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-21 08:21:41 +0000

    Sample: Walkthrough - Step 35 - Device Adaptation

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,34 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
-    "use strict";
-    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata: {
-            interfaces: ["sap.ui.core.IAsyncContentCreation"],
-            manifest: "json"
-        },
-        init: function () {
-            // call the init function of the parent
-            UIComponent.prototype.init.apply(this, arguments);
-            // set data model
-            var oData = {
-                recipient: {
-                    name: "World"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.setModel(oModel);
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], function (UIComponent, JSONModel, Device) {
+	"use strict";
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		},
+		init: function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
+			// set data model
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+			// disable batch grouping for v2 API of the northwind service
+			this.getModel("invoice").setUseBatch(false);
 
-            // create the views based on the url/hash
-            this.getRouter().initialize();
-        }
-    });
+			// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		}
+
+	});
 });
